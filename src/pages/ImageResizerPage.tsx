0f053373a7db22ef8b8b1a0e968c9bd50a7d5bd2
Ajmal <88565942+AjmalX0@users.noreply.github.com>
2026-05-15 15:24:05 +0530

    Create ImageResizerPage.tsx

---
+++ src/pages/ImageResizerPage.tsx
@@ -0,0 +1,9 @@
+import ImageResizer from "../components/ImageResizer";
+
+export default function ImageResizerPage() {
+  return (
+    <div className="p-6">
+      <ImageResizer />
+    </div>
+  );
+}
